7f19834d8e2bec5fc6e12d63295d45595c61a05a
milan-1936 <khpo_bca2077@lict.edu.np>
2026-05-05 20:49:01 +0545

    Day one End

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import card from './components/card'
+function user() {
+    return (
+        <div>
+            {/* Mistake......... */}
+            {card}
+        </div>
+    )
+}
+
+export default user
