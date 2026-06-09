7b49407b2cef908f2ebb62249d96cf26cd0ae037
Nabin Poudel <pnabin225@gmail.com>
2026-06-09 18:37:03 +0545

    Now Its time to API calling

--- localStorage/src/App.jsx
+++ localStorage/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Lets do something that really makes sense.
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+
+    localStorage.setItem('name', 'John Doe')
+    const name = localStorage.getItem('name')
+    console.log(name)
+
+    const user = {
+        name: 'John Doe',
+        age: 30
+    }
+
+    localStorage.setItem('user', JSON.stringify(user))
+    const userFromStorage = JSON.parse(localStorage.getItem('user'))
+    console.log(userFromStorage)
+
+    return (
+        <div>
+        </div>
+    )
 }
 
 export default App
